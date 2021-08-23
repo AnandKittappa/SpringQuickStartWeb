612d96a09d2416f3959fffc94da13d2e5b170003
AnandKittappa <anandkathirai@gmail.com>
2021-08-23 18:44:09 -0400

    initial commit

---
+++ Pelican/src/test/java/com/visteon/eit/Pelican/PelicanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visteon.eit.Pelican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelicanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
